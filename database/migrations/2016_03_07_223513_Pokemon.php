660ce8f13f150895e8a768af217c24c5f6c6f50a
vagrant <vagrant@homestead>
2016-03-08 05:54:15 +0000

    Updated database to have only 'special' as well added seeds for the first 10 pokemon

--- database/migrations/2016_03_07_223513_Pokemon.php
+++ database/migrations/2016_03_07_223513_Pokemon.php
@@ -17,12 +17,12 @@ class Pokemon extends Migration
             $table->string('name');
             $table->string('type_1');
             $table->string('type_2')->nullable();
-            $table->double('hp', 15, 8);
-            $table->double('attack', 15, 8);
-            $table->double('defense', 15, 8);
-            $table->double('special_attack', 15, 8);
-            $table->double('special_defense', 15, 8);
-            $table->double('catch_rate', 15, 8);
+            $table->double('hp', 3, 1);
+            $table->double('attack', 3, 1);
+            $table->double('defense', 3, 1);
+            $table->double('special', 3, 1);
+            $table->double('speed', 3, 1);
+            $table->double('catch_rate', 3, 1);
         });
     }
     /**
--- database/seeds/PokemonsTableSeeder.php
+++ database/seeds/PokemonsTableSeeder.php
@@ -11,12 +11,28 @@ class PokemonsTableSeeder extends Seeder
      */
     public function run()
     {
-     DB::insert(
+        $poke_array = [
+            [1,"Bulbasaur","Grass","Poison",45,49,49,65,45,45],
+            [2,"Ivysaur","Grass","Poison",60,62,63,80,60,45],
+            [3,"Venusaur","Grass","Poison",80,82,83,100,80,45],
+            [4,"Charmander","Fire",null,39,52,43,50,65,45],
+            [5,"Charmeleon","Fire",null,58,64,58,65,80,45],
+            [6,"Charizard","Fire","Flying",78,84,78,85,100,45],
+            [7,"Squirtle","Water",null,44,48,65,50,43,45],
+            [8,"Wartortle","Water",null,59,63,80,65,58,45],
+            [9,"Blastoise","Water",null,79,83,100,85,78,45],
+            [10,"Caterpie","Bug",null,45,30,35,20,45,255]
+            ];
+
+
+        foreach ($poke_array as list($a,$b,$c,$d,$e,$f,$g,$h,$i,$j))
+        {
+            DB::insert(
             'insert into pokemons 
-            (id, name, type_1, type_2, hp, attack, defense, special_attack, special_defense,
-            catch_rate) 
+            (id, name, type_1, type_2, hp, attack, defense, special, speed, catch_rate) 
             values 
             (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ', 
-            [25, "Pikachu", "Electric", null, 35, 55, 30, 50, 40, 90]);
+             [$a,$b,$c,$d,$e,$f,$g,$h,$i,$j]);
+        }
     }
 }
