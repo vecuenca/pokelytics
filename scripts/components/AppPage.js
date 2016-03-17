47f7f0c70e4618426bc5ccd99b6883b81e92fdc5
Jack Yiu <jackinyiu@gmail.com>
2016-03-17 16:00:19 -0400

    added sidebar

--- scripts/components/AppPage.js
+++ scripts/components/AppPage.js
@@ -10,6 +10,7 @@ import AvPlaylistAdd from 'material-ui/lib/svg-icons/av/playlist-add'
 import EditorFormatListNumbered from 'material-ui/lib/svg-icons/editor/format-list-numbered'
 import SocialGroup from 'material-ui/lib/svg-icons/social/group'
 import MapsPlace from 'material-ui/lib/svg-icons/maps/place'
+import ActionInfo from 'material-ui/lib/svg-icons/action/info';
 
 import QueryBox from './QueryBox'
 import request from 'superagent';
@@ -63,16 +64,62 @@ class AppPage extends React.Component {
 
 	render() {
 		return (
-			<div className="LandingPage">
-				<h2 style={style.title}>Pokélytics</h2>
-				<h5 style={style.tagLine} className="muted">Gotta analyze them all</h5>
-				<div style={style.query}>
-					<QueryBox
-						onSubmit={this.onSubmit.bind(this)}
-						onChange={this.onChange.bind(this)}></QueryBox>
-				</div>
-				<div style={style.results}>
-					{this.state.err ? this.state.err : this.state.res }
+			<div>
+				<LeftNav>
+					<List subheader="Table Schemas">
+						<ListItem
+							primaryText="Pokemons" 
+							//secondaryText="Pokemon name, stats"
+							rightIcon={<ActionInfo />}/>
+						<ListItem
+							primaryText="Trainers" 
+							//secondaryText="Trainers' name, geo-location"
+							rightIcon={<ActionInfo />}/>
+						<ListItem
+							primaryText="Moves" 
+							//secondaryText="Moves' name, stats"
+							rightIcon={<ActionInfo />}/>
+						<ListItem
+							primaryText="Locations" 
+							//secondaryText="In-Game Locations"
+							rightIcon={<ActionInfo />}/>
+						<ListItem
+							primaryText="Pokemon Moves" 
+							//secondaryText="Moves that pokemon can learn"
+							rightIcon={<ActionInfo />}/>
+						<ListItem
+							primaryText="Pokemon Locations" 
+							//secondaryText="Locations where pokemon can be found"
+							rightIcon={<ActionInfo />}/>
+						<ListItem
+							primaryText="Pokemon Trainers" 
+							//secondaryText="Pokemon Trainer's team of pokemon'"
+							rightIcon={<ActionInfo />}/>
+					</List>
+					<List subheader="Predefined SQL Queries">
+						<ListItem
+							onClick={this.allPokemonClick.bind(this)}
+							primaryText="All Pokemon" 
+							leftIcon={<EditorFormatListNumbered></EditorFormatListNumbered>}/>
+						<ListItem
+							primaryText="Trainers" 
+							leftIcon={<SocialGroup></SocialGroup>}/>
+						<ListItem
+							primaryText="Pokemon Locations"       
+							leftIcon={<MapsPlace></MapsPlace>}/>
+					</List>
+				</LeftNav>
+				<div style={style.AppArea}>
+					<h2 style={style.title}>Pokélytics</h2>
+					<h5 style={style.tagLine} className="muted">Gotta analyze them all</h5>
+					<div style={style.query}>
+						<QueryBox
+							onSubmit={this.onSubmit.bind(this)}
+							onChange={this.onChange.bind(this)}></QueryBox>
+					</div>
+					<div style={style.results}>
+						{this.state.err ? this.state.err : this.state.res }
+					</div>
 				</div>
 			</div>
 			);
@@ -88,6 +135,7 @@ const style = {
 		fontSize: '25px',
 		marginTop: '0',
 		marginBottom: '20px',
+		textAlign: 'center'
 	},
 	AppArea: {
 		display: 'block',
@@ -97,12 +145,18 @@ const style = {
 	},
 	query: {
 		display: 'block',
-		width: '500px'
+		maxWidth: '500px',
+		width: '100%',
+		margin: '0 auto',
+		marginBottom: '30px'
 	},
 	results: {
 		display: 'block',
 		marginTop: '25px',
-		width: '500px'
+		maxWidth: '800px',
+		width: '100%',
+		overflow: 'scroll',
+		margin: '0 auto'
 	}
 }
 
