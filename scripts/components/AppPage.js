d789cb2d67a3dcd2a4740a3de5e0cd78defb1b8a
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2016-03-17 19:14:19 -0400

    Added Schema dialogs

--- scripts/components/AppPage.js
+++ scripts/components/AppPage.js
@@ -11,6 +11,7 @@ import EditorFormatListNumbered from 'material-ui/lib/svg-icons/editor/format-li
 import SocialGroup from 'material-ui/lib/svg-icons/social/group'
 import MapsPlace from 'material-ui/lib/svg-icons/maps/place'
 import ActionInfo from 'material-ui/lib/svg-icons/action/info';
+import Dialog from 'material-ui/lib/dialog';
 
 import QueryBox from './QueryBox'
 import request from 'superagent';
@@ -22,7 +23,10 @@ class AppPage extends React.Component {
 		this.state = {
 			query: '',
 			res: [],
-			err: ""
+			err: "",
+			open: false,
+			dialog: "",
+			dialogTitle: ""
 		}
 	}
 
@@ -62,50 +66,132 @@ class AppPage extends React.Component {
 		}))
 	}
 
+	handleOpen = (dialog, dialogTitle) => {
+    this.setState({open: true, dialog: dialog, dialogTitle: dialogTitle});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
 	render() {
+		const dialogData = [
+		["Pokemon Schema",
+		"Columns:id	int(10) UN AI PK \r\n \
+		name	varchar(255) \r\n \
+		type_1	varchar(255) \r\n \
+		type_2	varchar(255) \r\n \
+		hp	double(4,1) \r\n \
+		attack	double(4,1) \r\n \
+		defense	double(4,1) \r\n \
+		special	double(4,1) \r\n \
+		speed	double(4,1) \r\n \
+		catch_rate	double(4,1)"],
+		["Trainer Schema",
+		"Columns: \r\n \
+		id	int(10) UN AI PK \r\n \
+		name	varchar(255) \r\n \
+		physical_location	varchar(255)"],
+		["Moves Schema",
+		"Columns: \r\n \
+		id	int(10) UN AI PK \r\n \
+		name	varchar(255) \r\n \
+		type	varchar(255) \r\n \
+		category	varchar(255) \r\n \
+		power	int(11) \r\n \
+		accuracy	int(11) \r\n \
+		pp	int(11) \r\n \
+		effect	varchar(255)"],
+		["Locations Schema",
+		"Columns: \
+		id	int(10) UN AI PK \
+		name	varchar(255) \
+		region	varchar(255) \
+		music_played	varchar(255)"],
+		["Pokemon Moves Schema",
+		"Columns: \r\n \
+		pokemon	int(10) UN PK \r\n \
+		move	int(10) UN PK \r\n \
+		learned_at	int(11) PK"],
+		["Pokemon Locations Schema",
+		"Columns: \r\n \
+		pokemon	int(10) UN \r\n \
+		location	int(10) UN"],
+		["Pokemon Trainers Schema",
+		"Columns: \r\n \
+		pokemon	int(10) UN PK \r\n \
+		trainer	int(10) UN PK \r\n \
+		name	varchar(255) PK \r\n \
+		move1	int(10) UN \r\n \
+		move2	int(10) UN \r\n \
+		move3	int(10) UN \r\n \
+		move4	int(10) UN"]
+		]
+
+		var dialogStyle = {
+			whiteSpace: 'pre-wrap'
+		};
+
 		return (
 			<div>
 				<LeftNav>
 					<List subheader="Table Schemas">
+						<Dialog
+		          title={this.state.dialogTitle}
+		          modal={false}
+		          open={this.state.open}
+		          onRequestClose={this.handleClose}
+		          style={dialogStyle}
+		        >
+		        {this.state.dialog}
+        		</Dialog>
+
 						<ListItem
-							primaryText="Pokemons" 
+							primaryText="Pokemons"
+							onTouchTap={this.handleOpen.bind(this, dialogData[0][1], dialogData[0][0])}
 							//secondaryText="Pokemon name, stats"
 							rightIcon={<ActionInfo />}/>
 						<ListItem
-							primaryText="Trainers" 
+							primaryText="Trainers"
+							onTouchTap={this.handleOpen.bind(this, dialogData[1][1], dialogData[1][0])}
 							//secondaryText="Trainers' name, geo-location"
 							rightIcon={<ActionInfo />}/>
 						<ListItem
-							primaryText="Moves" 
+							primaryText="Moves"
+							onTouchTap={this.handleOpen.bind(this, dialogData[2][1], dialogData[2][0])}
 							//secondaryText="Moves' name, stats"
 							rightIcon={<ActionInfo />}/>
 						<ListItem
-							primaryText="Locations" 
+							primaryText="Locations"
+							onTouchTap={this.handleOpen.bind(this, dialogData[3][1], dialogData[3][0])}
 							//secondaryText="In-Game Locations"
 							rightIcon={<ActionInfo />}/>
 						<ListItem
-							primaryText="Pokemon Moves" 
+							primaryText="Pokemon Moves"
+							onTouchTap={this.handleOpen.bind(this, dialogData[4][1], dialogData[4][0])}
 							//secondaryText="Moves that pokemon can learn"
 							rightIcon={<ActionInfo />}/>
 						<ListItem
-							primaryText="Pokemon Locations" 
+							primaryText="Pokemon Locations"
+							onTouchTap={this.handleOpen.bind(this, dialogData[5][1], dialogData[5][0])}
 							//secondaryText="Locations where pokemon can be found"
 							rightIcon={<ActionInfo />}/>
 						<ListItem
-							primaryText="Pokemon Trainers" 
+							primaryText="Pokemon Trainers"
+							onTouchTap={this.handleOpen.bind(this, dialogData[6][1], dialogData[6][0])}
 							//secondaryText="Pokemon Trainer's team of pokemon'"
 							rightIcon={<ActionInfo />}/>
 					</List>
 					<List subheader="Predefined SQL Queries">
 						<ListItem
-							onClick={this.allPokemonClick.bind(this)}
-							primaryText="All Pokemon" 
+							onClick={this.onSubmit.bind('SELECT * from Pokemons')}
+							primaryText="All Pokemon"
 							leftIcon={<EditorFormatListNumbered></EditorFormatListNumbered>}/>
 						<ListItem
-							primaryText="Trainers" 
+							primaryText="Trainers"
 							leftIcon={<SocialGroup></SocialGroup>}/>
 						<ListItem
-							primaryText="Pokemon Locations"       
+							primaryText="Pokemon Locations"
 							leftIcon={<MapsPlace></MapsPlace>}/>
 					</List>
 				</LeftNav>
@@ -124,8 +210,8 @@ class AppPage extends React.Component {
 			</div>
 			);
 	}
-}
 
+}
 const style = {
 	title: {
 		fontSize: '56px',
