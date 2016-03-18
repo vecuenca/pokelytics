74b01c2102b3c9fa5bc4ec7ba8f1e3b88ef11a45
vagrant <vagrant@homestead>
2016-03-18 14:19:49 +0000

    Added location seeder, fixed pokemon moves seeder

--- database/migrations/2016_03_07_214247_Location.php
+++ database/migrations/2016_03_07_214247_Location.php
@@ -16,7 +16,6 @@ class Location extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('region');
-            $table->string('music_played');
         });
     }
 
