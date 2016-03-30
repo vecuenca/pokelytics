b8667bf85663e71f90136756e61e7b1cdda3efee
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2016-03-30 16:27:42 -0400

    Added Trainers pokemon dialog

--- scripts/components/NavMenu.js
+++ scripts/components/NavMenu.js
@@ -17,6 +17,9 @@ import MapsPlace from 'material-ui/lib/svg-icons/maps/place'
 import ActionInfo from 'material-ui/lib/svg-icons/action/info';
 import ActionHome from 'material-ui/lib/svg-icons/action/home';
 import ImageFlashOn from 'material-ui/lib/svg-icons/image/flash-on';
+import Group from 'material-ui/lib/svg-icons/social/group';
+import WhatsHot from 'material-ui/lib/svg-icons/social/whatshot';
+import GroupWork from 'material-ui/lib/svg-icons/action/group-work';
 
 import * as Schema from '../constants/Schema'
 
@@ -32,14 +35,18 @@ class NavMenu extends React.Component {
 			// Pokemon Moves Dialog
 			openPokemonMovesDialog: false,
 			moveDiagOpen: false,
-			pokemonList: []
+			pokemonList: [],
+			// Pokemon Trainers Dialog
+			openPokemonTrainersDialog: false,
+			trainerDiagOpen: false,
+			trainerList: []
 		}
 	}
 
 	openTableSchemaDialog(dialogContent, dialogTitle) {
 		this.setState({
-			openTableSchemaDialog: true, 
-			tableSchemaDialogContent: dialogContent, 
+			openTableSchemaDialog: true,
+			tableSchemaDialogContent: dialogContent,
 			tableSchemaDialogTitle: dialogTitle
 		})
 	}
@@ -77,6 +84,33 @@ class NavMenu extends React.Component {
 		})
 	}
 
+	// Populate autocomplete's data source
+	openPokemonTrainersDialog() {
+		// Get pokemon names and store it in state
+		this.props.query("SELECT name from trainers").then( res => {
+			var json = JSON.parse(res);
+			json.shift();
+			var result = [];
+
+			for (var i in json)
+				result.push(json[i].name);
+
+			this.setState({
+				trainerList: result
+			})
+		});
+
+		this.setState({
+			openPokemonTrainersDialog: true,
+		})
+	}
+
+	closePokemonTrainersDialog() {
+		this.setState({
+			openPokemonTrainersDialog: false
+		})
+	}
+
 	getPokemonMovesDialogActions() {
 		return [
 			<FlatButton
@@ -93,13 +127,44 @@ class NavMenu extends React.Component {
 		]
 	}
 
+	getPokemonTrainersDialogActions() {
+		return [
+			<FlatButton
+			      label="Cancel"
+			      secondary={true}
+			      onTouchTap={this.closePokemonTrainersDialog.bind(this)}
+			/>,
+		      <FlatButton
+				label="Submit"
+				primary={true}
+				keyboardFocused={true}
+				onTouchTap={this.handlePokemonTrainersDialogSubmit.bind(this)}
+		      />,
+		]
+	}
+
 	handlePokemonMovesDialogSubmit() {
 		// run the query here
 		let val = this.refs['pkmnMoveDiag'].getValue();
 		var query = "SELECT m.*, pm.learned_at from pokemons p, pokemon_moves pm, moves m  WHERE pokemon = p.id and move = m.id and p.name='" + val + "'";
 		this.props.displayAndSubmitQuery(query);
-		
+
 		this.closePokemonMovesDialog();
+	};
+
+	handlePokemonTrainersDialogSubmit() {
+		// run the query here
+		let val = this.refs['pkmnTrainerDiag'].getValue();
+		var id;
+
+		var res  = this.props.query("SELECT t.id from trainers t where t.name='" + val + "'").then( res => {
+			var json = JSON.parse(res);
+			json.shift();
+			id = json[0].id;
+			var query = "SELECT p.name, pt.name, pt.move1, pt.move2, pt.move3, pt.move4 from pokemons p, pokemon_trainers pt  WHERE pt.pokemon = p.id AND pt.trainer='" + id + "'";
+			this.props.displayAndSubmitQuery(query);
+			this.closePokemonTrainersDialog();
+		});
 	};
 
 	render() {
@@ -128,6 +193,25 @@ class NavMenu extends React.Component {
 						triggerUpdateOnFocus={true} />
 				</div>
 			</Dialog>
+			<Dialog
+				title='Choose a Trainer to view their Pokemon'
+				modal={false}
+				actions={this.getPokemonTrainersDialogActions()}
+				open={this.state.openPokemonTrainersDialog}
+				onRequestClose={this.closePokemonTrainersDialog.bind(this)}
+				style={dialogStyle}>
+				<div>
+					<AutoComplete
+						ref='pkmnTrainerDiag'
+						hintText="Enter Trainer name"
+						filter={AutoComplete.fuzzyFilter}
+						dataSource={this.state.trainerList}
+						triggerUpdateOnFocus={true} />
+				</div>
+			</Dialog>
+
+
+
 			<List>
 				<ListItem
 					onClick={() => { window.location = '/' }}
@@ -191,17 +275,19 @@ class NavMenu extends React.Component {
 					leftIcon={<ImageFlashOn></ImageFlashOn>}/>
 				<ListItem
 					primaryText="Find a Trainer's Pokemon"
-					onClick="{this.openTrainerDialog.bind(this)}"
-					leftIcon={<ImageFlashOn></ImageFlashOn>}/>
+					onClick={this.openPokemonTrainersDialog.bind(this)}
+					leftIcon={<Group></Group>}/>
 			</List>
 			<Divider></Divider>
 			<List subheader="Analytics">
 				<ListItem
 					primaryText="Pokemon grouped by type"
-					onClick={() => { window.location='/graphs' }} />
+					onClick={() => { window.location='/graphs' }}
+					leftIcon={<GroupWork></GroupWork>}/>
 				<ListItem
 					primaryText="Most popular Pokemon"
-					onClick={() => { window.location='/Popularity' }} />
+					onClick={() => { window.location='/Popularity' }}
+					leftIcon={<WhatsHot></WhatsHot>}/>
 			</List>
 		</LeftNav>);
 	}
