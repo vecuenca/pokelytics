862ec2f806012166da74c5d4ebe84265ab2b6794
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2016-03-30 08:53:14 -0400

    Fixed PokemonTrainers seeder

--- database/seeds/PokemonTrainersTableSeeder.php
+++ database/seeds/PokemonTrainersTableSeeder.php
@@ -12,32 +12,27 @@ class PokemonTrainersTableSeeder extends Seeder
     public function run()
     {
         $ptrainer_array = [
-<<<<<<< HEAD
-            [1, 1, "Bulbasaur", 1, 2, 3, 4],
-            [151, 1, "Mewtwo", 1, 2, 3, 4],
-            [6, 1, "Charizard", 6, 21, 10, 32],
-            [51,1,"DugT",8,16,21,15],
-            [59,1,"FireDog",11,24,16,33],
-            [65,1,"Psychic Dude",24,36,11,10],
-            
-            [9, 2, "Blastoise", 1, 2, 3, 4],
-            [151, 2, "Mewtwo", 6, 10, 18, 33],
-            [10, 2, "Caterpie", 6, 21, 10, 3],
-            [55,2,"GOLduck",8,16,21,15],
-            [89,2,"MUK",11,24,16,33],
-            [65,2,"Psychic Dude",24,36,11,10],
-            
-            [18, 3, "Pidgeot", 1, 2, 3, 4],
-            [151, 3, "Mewtwo", 6, 10, 18, 33],
-            [26, 3, "Raichu", 6, 21, 10, 3],
-            [51,3,"Dugtrio",8,16,21,15],
-            [63,3,"Abra",11,24,16,33],
-            [107,3,"Dude",24,36,11,10]
+            [1, 1, "Bulbasaur", 13, 14, 15, 20],
+            [151, 1, "Mewtwo", 1, 5, 6, 7],
+            [6, 1, "Charizard", 10, 5, 7, 9],
+            [51,1,"DugT",10,15,28,34],
+            [59,1,"FireDog",29,34, 36, 38],
+            [65,1,"Psychic Dude",5,7,8,9],
+
+            [9, 2, "Blastoise", 5, 8, 25, 29],
+            [151, 2, "Mewtwo", 6, 7, 8, 9],
+            [12, 2, "Caterpie", 13, 16, 18, 36],
+            [55,2,"GOLduck",5,6,25,129],
+            [89,2,"MUK",1,7,8,9],
+            [65,2,"Psychic Dude",5, 7, 8, 9],
+
+            [18, 3, "Pidgeot", 13, 16, 17, 18],
+            [151, 3, "Mewtwo", 6, 7,8,9],
+            [26, 3, "Raichu", 5,6,9,25],
+            [51,3,"Dugtrio",10,15,28,34],
+            [63,3,"Abra",5,7,8,9],
+            [107,3,"Dude",4,5,7,8]
             ];
-=======
-            [1, 1, "Vassos", 13, 14, 15, 20]
-        ];
->>>>>>> origin/master
 
         foreach ($ptrainer_array as list($a,$b,$c,$d,$e,$f,$g))
         {
